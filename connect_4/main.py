74aaa61bbf52782b5cc62a48cf36241b3ecb0a0a
denebola <ojadekola@gmail.com>
2025-11-13 13:31:14 +0000

    Incorporated game design  image Connect4_screen.png created in powerpoint into project. Made small tweaking changes to main.py incorporating dunder, 'if __name__ ...' Made changes to constants.py adding additional colors and constants reflecting changes made in board.py Made changes to the draw_board method of the Board class  to divide the window into two sections, a game_section and an info_section

--- connect_4/main.py
+++ connect_4/main.py
@@ -1,38 +1,34 @@
 import pygame
+from pygame.locals import *
 from Game.constants import WIDTH, HEIGHT
 from Game.board import Board
 
 FPS = 120
-# Set display mode and caption for window
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Connect 4")
 
-# Set event loop
 def main():
+    pygame.init()
+    WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("Connect 4")
+
     run = True
-    clock = pygame.time.Clock(FPS)
+    clock = pygame.time.Clock()
     board = Board()
-    
+
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                
+
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
-                print(f"Mouse clicked at position: {pos}")  
-                
-                
-                
+                print(f"Mouse clicked at position: {pos}")
+
         board.draw_board(WIN)
-        pygame.display.update()  
-          
+        pygame.display.update()
+
     pygame.quit()
 
 
-        
-
-
-# if __name__ == "__main__":  
-main()
+if __name__ == "__main__":
+    main()
