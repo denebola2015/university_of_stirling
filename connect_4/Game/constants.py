a7389b19bc658c51ecfeafd2aea6d1d71c232643
denebola <ojadekola@gmail.com>
2025-11-15 11:24:21 +0000

    Added Constants for Button dimensions, positions, and colors

--- connect_4/Game/constants.py
+++ connect_4/Game/constants.py
@@ -1,17 +1,22 @@
+"""This module defines constants used in the Connect 4 game, including dimensions, colors, and frame rate. """
 import pygame
  
 WIDTH, HEIGHT = 800, 700
- 
+ # rows and columns are set to the standard Connect 4 dimensions and the requirements for the assessment
 ROWS, COLS = 6, 7
-COLUMN_SIZE = 600 // COLS
-ROW_SIZE = HEIGHT // ROWS
-TOKEN_RADIUS = min(COLUMN_SIZE, ROW_SIZE) // 2 - 10
+COLUMN_SIZE = 600 // COLS 
+# Divides 600px(the width less the area architected for the info section) by number of columns in order to calculate column size 
+ROW_SIZE = (HEIGHT - 200) // ROWS 
+#Row size, subtracts 200px to account for info section
+TOKEN_RADIUS = 20
+# The radius of each game piece
 FPS = 120
 CIRCLE_SIZE = (40, 40)
+# Size of each piece when drawn on the board
 
 # RGB COLORS
 RED = (255, 0, 0)
-YELLOW = (255, 255, 0)
+YELLOW = (255, 255, 0) 
 BLUE = (65,105,225)
 SKY_BLUE = (135,206,235)
 WHITE = (255, 255, 255)
